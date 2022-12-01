dc54a1ce0b53b4e25a58cf4de07c4ed015351ad6
Pero5511 <pericatopic5511@gmail.com>
2022-12-01 14:38:10 +0100

    Martin 01122022 + ja 01122022

--- index.php
+++ index.php
@@ -1,13 +1,107 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-  <title>PROJEKT</title>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-</head>
-<body>  
-    <p>Hello World!!!</p>
-  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-</body>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>E-Sweepstakes</title>
+    <link rel="icon" href="./img/favicon.ico">
+  </head>
+  <body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col">
+            <div class="jumbotron bg-dark text-white jumbotron-fluid">
+                <div class="container">
+                <h1 class="display-4">E-Sweepstakes</h1>
+                <hr class="my-4">
+                </div>
+            </div>
+                <br>
+            </div>
+        </div>
+        <div class="container">
+        <div class="row">
+            <div class="col">
+                <p>
+                    Ovo je naslovna stranica projekta iz kolegija Programiranje za internet kod profesora 
+                    dr. sc. Tomislava Volarić doc. Projekt se zove E-Sweepstakes i on je zamišljen kao internetska 
+                    stranica na kojoj će se moći igrati i pratiti nagradne igre. Tehnologije koje planiramo koristiti 
+                    u projektu su PHP Laravel, MySql, Apache.
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <p>
+                    Projekt je nastao kao ideja da se sve nagradne igre nalaze na jednom
+                    mjestu, te da korisnik može sve imati na jednom mjestu. Organizatorima 
+                    nudi pregled svih igara. Korisnik može pronaći kategoriju igre koju želi.
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <p>
+                    Svakome se barem jednom desilo da je propustio nagradnu igru ili da nikada nije saznao
+                    na nju. Te nam je cilj ovog projekta bio napraviti web stranicu na kojoj će se pratiti sve 
+                    aktualne nagradne igre, koje se nalaze na jednom mjestu te su prijave na njih vrlo jednostavne, 
+                    što će privući mnogo korisnika koji će nakon jedne odigrane igre ostat duže vrijeme.
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 text-center">
+                <a align:center class="btn btn-dark" href="https://github.com/Pero5511/E-Sweepstakes/blob/main/Vizija.docx?raw=true">Link vizije</a>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-md-3 col-xs-12 col-sm-12">
+                <img src="./img/perica.jpg" alt="" width="200">               
+            </div>
+            <div class="col-md-3 col-xs-12 col-sm-12">
+                <p>
+                    Zovem se Perica Topić, dolazim iz peteročlane obitelji. 
+                    Rođen sam u Slavonskom Brodu. Prešao sam iz osječkog FERIT-a 
+                    na SUM zbog novonastale situacije sa koronavirusom. Trenutno studiram 
+                    treću godinu na Mostarskom Sveučilištu, studij Informatika u dislociranom 
+                    centru Orašje. Osnovnu školu sam pohađao u svom mjestu, pod nazivom "Osnovna 
+                    škola Stjepana Radića". Srednju školu sam pohađao u glavnom gradu općine - Orašju,
+                    pod nazivom "Srednjoškolski centar fra Martina Nedića“. U slobodno vrijeme se bavim 
+                    trčanjem, volim šah, kvizove i društvene igre.
+                </p>
+                <a class="btn btn-dark" href="https://github.com/Pero5511" target="_blank">Github link</a>
+            </div>
+            <div class="col-md-3 col-xs-12 col-sm-12">
+                <img src="./img/martin.png" alt="" width="200">
+            </div>
+            <div class="col-md-3 col-xs-12 col-sm-12">
+                <p>
+                    Zovem se Martin Karalić. Rođen sam u Vinkovcima 18. rujna 2001. godine,
+                    te trenutno imam 21 godinu.Osnovnu školu sam završio u Cerni pod nazivom Osnovna 
+                    škola Matije Antun Reljković. Srednju školu sam pohađao u rodnom gradu i zove se 
+                    Tehnička škola Ruđera Bošković Vinkovci te završio za Tehničara za mehatroniku. 
+                    Trenutno studiram na Sveučilištu u Mostaru, FPMOZ studij Informatike. Neki od mojih 
+                    hobija su folklor i sviranje u TS Birtaši.
+                </p>
+                <a class="btn btn-dark" href="https://github.com/mkaralic1" target="_blank">Github link</a>
+            </div>
+            </div>     
+        </div>
+        <hr>
+        <div class="container-fluid">
+        <div class="row">
+            <div class="col">
+            <div class="jumbotron bg-dark text-white jumbotron-fluid">
+                <hr class="my-4">
+                <div class="container">
+                <h6 align="center" class="display-6">FPMOZ - 2022</h6>
+                </div>
+            </div>
+                <br>
+            </div>
+        </div>
+    </div>
+  </body>
 </html>
